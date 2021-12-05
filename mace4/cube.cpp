432cf7dc2b6923a2453cba6f3afa178d14f8eb91
uabcoimbra <choiwah.chow@gmail.com>
2021-12-05 12:02:16 -0500

    removed commented out code

--- mace4/cube.cpp
+++ mace4/cube.cpp
@@ -24,34 +24,6 @@ Cube::Cube(): initialized(false) {
 	config.close();
 	initialized = true;
 	std::cout << "\ndebug Cube*********************** max_depth = " << cell_values.size() - 1<< std::endl;
-
-
-	/*
-	size_t id = 0;
-	bool base_initialized = false;
-	while( !base_initialized ) {
-		std::string& this_op = Symbol_dataContainer::get_op_symbol(Cells[id].get_sn());
-		if (this_op == op) {
-			base = Cells[id].get_base();
-			base_initialized = true;
-		}
-		id ++;
-	}
-	int cell_id;
-	int cell_value;
-	config >> cell_id;
-	while (cell_id >= 0) {
-		cell_ids.push_back(cell_id + base);
-		config >> cell_value;
-		cell_values.push_back(cell_value);
-		config >> cell_id;
-	}
-	config.close();
-	std::cout << "debug Cube***********************base = " << base << ", number of cells = " << cell_values.size() << std::endl;
-	if (cell_ids.size() == 0)
-		return;
-	cell_id += base;
-	*/
 }
 
 
@@ -60,11 +32,4 @@ Cube::value(size_t depth) {
 	if (initialized && cell_values.size() > depth)
 		return -1;
 	return cell_values[depth];
-	/*
-	for( size_t idx = 0; idx < cell_ids.size(); ++idx ) {
-		if (cell_ids[idx] == id)
-			return cell_values[idx];
-	}
-	return -1;
-	*/
 }
